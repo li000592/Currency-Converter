c2fbaab98fa30725f2216edf585f9297178a8464
“Haorong <li000592@algonquinlive.com>
2020-11-19 14:51:37 -0500

    feat: create initialize structure

---
+++ src/ExchangeCurrencyListPage/AddCountryPage/AddCountryPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AddCountryPage() {
+  return <div></div>;
+}
+
+export default AddCountryPage;
---
+++ src/ExchangeCurrencyListPage/EchangeCurrencyListPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function EchangeCurrencyListPage() {
+  return <div></div>;
+}
+
+export default EchangeCurrencyListPage;
