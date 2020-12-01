472692e83380de80e91e998b845f0077f31cd80c
cao00096@algonquinlive.com <cao00096@algonquinlive.com>
2020-11-30 20:35:02 -0500

    resovle confict with main branch

--- src/components/PWAInstallation/InstallPwa.js
+++ src/components/PWAInstallation/InstallPwa.js
@@ -1,43 +1,58 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from "react"
 import { NavLink } from 'react-router-dom'
 import './InstallPwa.css'
 
+
 const InstallPWA = () => {
-  const [supportsPWA, setSupportsPWA] = useState(false)
-  const [promptInstall, setPromptInstall] = useState(null)
+  const [supportsPWA, setSupportsPWA] = useState(false);
+  const [promptInstall, setPromptInstall] = useState(null);
 
   useEffect(() => {
     const handler = e => {
-      e.preventDefault()
-      console.log('we are being triggered :D')
-      setSupportsPWA(true)
-      setPromptInstall(e)
+      e.preventDefault();
+      console.log("we are being triggered :D");
+      setSupportsPWA(true);
+      setPromptInstall(e);
     }
-    window.addEventListener('beforeinstallprompt', handler)
+    window.addEventListener("beforeinstallprompt", handler);
 
-    return () => window.removeEventListener('transitionend', handler)
+    return () => window.removeEventListener("transitionend", handler);
   }, [])
 
   const onClick = evt => {
-    evt.preventDefault()
+    evt.preventDefault();
     if (!promptInstall) {
-      return
+      return;
     }
-    promptInstall.prompt()
+    promptInstall.prompt();
   }
   if (!supportsPWA) {
-    return null
+    return null;
   }
   return (
-    <NavLink
-      className="link-button"
+    <div className='pwaInstallPrompt'>
+      <div className='promptCard'>
+        <p className ='promptTxt'>Installing uses almost no storage and process quick way to return to this app</p>
+      <div className ='promptAction'>
+      <button
+      className="pwaButton"
       id="setup_button"
       aria-label="Install app"
       title="Install app"
       onClick={onClick}
     >
       Install
-    </NavLink>
+    </button>
+
+    <NavLink to='./' className='later'> Later </NavLink>
+      </div>
+      
+      </div>
+
+
+    </div>
+
+
   )
 }
 
