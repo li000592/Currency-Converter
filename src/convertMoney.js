49239ca3cd368b7c39f163412afbcd6d832ad90c
“Haorong <li000592@algonquinlive.com>
2020-11-30 19:08:37 -0500

    improve data saving and using way

--- src/convertMoney.js
+++ src/convertMoney.js
@@ -1,17 +1,16 @@
 import getData, {getCurrenciesLongName} from './getData'
 
-export async function convertMoney(shortName, setNumber) {
-  console.log(shortName, setNumber)
-  const ratesList = await convertData()
-  shortName = shortName.toUpperCase()
-  console.log(ratesList)
-  const index = ratesList.findIndex(obj => obj.shortName === shortName)
-  if (index === -1) {
-    return console.warn('FIX IT LATER')
+export async function convertMoney(index, setNumber) {
+  let ratesList = await convertData()
+  if (typeof ratesList === 'string') {
+    ratesList = JSON.parse(ratesList)
   }
+  console.log(ratesList)
+
   const percentage = setNumber / ratesList[index].exchangeNumber
   return ratesList.map(rate => {
     return {
+      index: rate.index,
       shortName: rate.shortName,
       fullName: rate.fullName,
       exchangeNumber: rate.exchangeNumber * percentage,
@@ -19,30 +18,34 @@ export async function convertMoney(shortName, setNumber) {
   })
 }
 export const convertData = async () => {
-  let newRatesList = []
-  const currencyFullName = await getCurrenciesLongName()
-  window.localStorage.setItem(
-    'currencyFullName',
-    JSON.stringify(currencyFullName),
-  )
-  console.log(currencyFullName)
-  //let ratesList = await getData().then(repsonse => JSON.parse(repsonse).rates)
-  let ratesList = await getData().then(repsonse => JSON.parse(repsonse))
-  console.log(ratesList)
-  const key = await Object.keys(ratesList)
-  console.log(key)
-  const values = await Object.values(ratesList)
-  console.log(values)
-  for (let i = 0; i < key.length; i++) {
-    const shortName = key[i]
-    newRatesList.push({
-      shortName: key[i],
-      fullName: currencyFullName.symbols[shortName],
-      exchangeNumber: values[i],
-    })
+  // get time
+  const dateNow = new Date()
+  const updateTime = localStorage.getItem('updateTime')
+  // getMoth start from 0, so +1
+  const convertNow = `${dateNow.getFullYear()}-${dateNow.getMonth() + 1}-${dateNow.getDate()}`
+  // get localStorage
+  const ratesList = localStorage.getItem('ratesList')
+  if (ratesList !== null && updateTime === convertNow) {
+    // use localstorage rateslist.
+    return ratesList
+  } else {
+    let newRatesList = []
+    const currencyFullName = await getCurrenciesLongName()
+    let ratesList = await getData().then(repsonse => JSON.parse(repsonse))
+    const key = await Object.keys(ratesList)
+    const values = await Object.values(ratesList)
+    for (let i = 0; i < key.length; i++) {
+      const shortName = key[i]
+      newRatesList.push({
+        index: i,
+        shortName: key[i],
+        fullName: currencyFullName.symbols[shortName],
+        exchangeNumber: values[i],
+      })
+    }
+    window.localStorage.setItem('ratesList', JSON.stringify(newRatesList))
+    return newRatesList
   }
-
-  return newRatesList
 }
 
 export default convertMoney
